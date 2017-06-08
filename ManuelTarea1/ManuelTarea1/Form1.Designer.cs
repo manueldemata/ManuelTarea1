97511cbc5456020ad514fee03e482ff9b57985ad
FERNANDA LOPEZ <FERNANDA LOPEZ@FERNANDA>
2017-06-07 18:49:02 -0600

    Primer Commit

---
+++ ManuelTarea1/ManuelTarea1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ManuelTarea1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSegundo = new System.Windows.Forms.TextBox();
+            this.txtTercero = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtPrimero = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Segundo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tercero";
+            // 
+            // txtSegundo
+            // 
+            this.txtSegundo.Location = new System.Drawing.Point(116, 124);
+            this.txtSegundo.Name = "txtSegundo";
+            this.txtSegundo.Size = new System.Drawing.Size(236, 22);
+            this.txtSegundo.TabIndex = 4;
+            // 
+            // txtTercero
+            // 
+            this.txtTercero.Location = new System.Drawing.Point(116, 189);
+            this.txtTercero.Name = "txtTercero";
+            this.txtTercero.Size = new System.Drawing.Size(236, 22);
+            this.txtTercero.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 73);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Hipotenusa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(561, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 73);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Vocales";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(390, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 73);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Invertir";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // txtPrimero
+            // 
+            this.txtPrimero.Location = new System.Drawing.Point(561, 138);
+            this.txtPrimero.Name = "txtPrimero";
+            this.txtPrimero.Size = new System.Drawing.Size(87, 73);
+            this.txtPrimero.TabIndex = 9;
+            this.txtPrimero.Text = "Regla De Tres";
+            this.txtPrimero.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 22);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 234);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPrimero);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTercero);
+            this.Controls.Add(this.txtSegundo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSegundo;
+        private System.Windows.Forms.TextBox txtTercero;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button txtPrimero;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ManuelTarea1/ManuelTarea1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ManuelTarea1
+{
+    public partial class Form1 : Form
+    {
+
+        double A, B, C;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            A = Convert.ToInt32(textBox1.Text);
+            B = Convert.ToInt32(txtSegundo.Text);
+            MessageBox.Show("La hipotenusa es " + Math.Sqrt((A * A) + (B * B)));
+        }
+    }
+}
